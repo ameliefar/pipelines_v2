dbcc8301433099624efa049f3368a78dea1fd67c
Amélie Fargevieille <afargevieille@gmail.com>
2024-11-08 18:08:18 +0100

    :construction: building test-BRG v2.0.0

--- tests/testthat/test-BRG.R
+++ tests/testthat/test-BRG.R
@@ -1,3 +1,147 @@
 testthat::skip_if(!exists("data_path"))
 
 pipeline_output <- format_BRG(db = paste0(data_path, "/BRG_Bergen_Norway"))
+
+
+test_that("BRG outputs all files...", {
+
+  expect_true(all("BRG" %in% pipeline_output$Brood_data$siteID))
+  expect_true(all("BRG" %in% pipeline_output$Capture_data$captureSiteID))
+  expect_true(all("BRG" %in% pipeline_output$Individual_data$siteID))
+  expect_true(all("BRG" %in% pipeline_output$Measurement_data$siteID))
+  expect_true(all("BRG" %in% pipeline_output$Location_data$siteID))
+#'  expect_true(all("BRG" %in% pipeline_output$Experiment_data$siteID)) #no Experiment table
+
+}) #test passed
+
+
+
+
+
+
+
+
+
+
+## General tests (for pipelines formatted to standard protocol version 2.0)
+#'Need to run test_general_format.R in folder R
+
+test_that("Expected columns are present", {
+  ## Will fail if not all the expected columns are present
+
+  ## Brood data: Test that all columns are present
+  test_col_present(pipeline_output, "Brood")
+
+  ## Capture data: Test that all columns are present
+  test_col_present(pipeline_output, "Capture")
+
+  ## Individual data: Test that all columns are present
+  test_col_present(pipeline_output, "Individual")
+
+  ## Measurement data: Test that all columns are present
+  test_col_present(pipeline_output, "Measurement")
+
+  ## Location data: Test that all columns are present
+  test_col_present(pipeline_output, "Location")
+
+  ## Experiment data: Test that all columns are present
+  test_col_present(pipeline_output, "Experiment")
+
+}) #test failed for Brood, Capture, Individual
+
+
+
+test_that("Key columns in each table do not have NAs", {
+
+  ## Brood
+  test_NA_columns(pipeline_output, "Brood")
+
+  ## Capture
+  test_NA_columns(pipeline_output, "Capture")
+
+  ## Individual
+  test_NA_columns(pipeline_output, "Individual")
+
+  ## Measurement
+  test_NA_columns(pipeline_output, "Measurement")
+
+  ## Location
+  test_NA_columns(pipeline_output, "Location")
+
+  ## Experiment
+  test_NA_columns(pipeline_output, "Experiment")
+
+}) #test failed for Capture (at least)
+
+
+test_that("Column classes are as expected", {
+
+  ## Will fail if columns that are shared by the output and the templates have different classes.
+
+  ## Brood data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Brood")
+
+  ## Capture data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Capture")
+
+  ## Individual data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Individual")
+
+  ## Measurement data: Test that all column classes are expected
+  test_col_present(pipeline_output, "Measurement")
+
+  ## Location data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Location")
+
+  ## Experiment data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Experiment")
+
+}) #test passed
+
+test_that("Key columns only contain unique values", {
+
+  ## broodID has only unique values
+  test_unique_values(pipeline_output, "broodID")
+
+  ## captureID has only unique values
+  test_unique_values(pipeline_output, "captureID")
+
+  ## individualID has only unique values
+  test_unique_values(pipeline_output, "individualID")
+
+  ## measurementID has only unique values
+  test_unique_values(pipeline_output, "measurementID")
+
+  ## locationID has only unique values
+  test_unique_values(pipeline_output, "locationID")
+
+  ## treatmentID has only unique values
+  test_unique_values(pipeline_output, "treatmentID")
+
+}) #test passed
+
+
+
+
+test_that("Categorical columns do not have unexpected values", {
+
+  ## Brood
+  test_category_columns(pipeline_output, "Brood")
+
+  ## Capture
+  test_category_columns(pipeline_output, "Capture")
+
+  ## Individual
+  test_category_columns(pipeline_output, "Individual")
+
+  ## Measurement
+  test_category_columns(pipeline_output, "Measurement")
+
+  ## Location
+  test_category_columns(pipeline_output, "Location")
+
+  ## Experiment
+  test_category_columns(pipeline_output, "Experiment")
+
+}) #Location table failed
+
